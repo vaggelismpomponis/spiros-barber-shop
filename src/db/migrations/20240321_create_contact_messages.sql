39cec1dcaefcbb16650fc35cf860c2efbf50a35d
Vaggelis Bobonhs <vaggelisbobonhs@gmail.com>
2025-04-20 04:10:10 +0300

    Add trigger function definition to contact messages migration

--- src/db/migrations/20240321_create_contact_messages.sql
+++ src/db/migrations/20240321_create_contact_messages.sql
@@ -1,3 +1,12 @@
+-- Create trigger function for updating timestamps
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = TIMEZONE('utc', NOW());
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 -- Create contact_messages table
 CREATE TABLE IF NOT EXISTS contact_messages (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
